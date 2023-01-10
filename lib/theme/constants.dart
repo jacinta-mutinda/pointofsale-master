aafd9e7a732992469109527ecbed4e80251259ee
mel-fayne <melaniefayne33@gmail.com>
2023-01-10 09:45:30 +0300

    import project assets

---
+++ lib/theme/constants.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const kDarkGreen = Color(0xFF054D3B);
+const kNeonGreen = Color(0xFF2AB514);
+const kLightGreen = Color(0xFF8FBD4F);
+const kGrey = Color(0xFFF5F5F5);
+
+const kPageTitle = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: Colors.white);
+    
+const kTitle = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: kDarkGreen);
+
+const kSubTitle = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: kDarkGreen);
+
+const kHighlight = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: kNeonGreen);
+
+const kBlackTxt = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.black);
+
+const kWhiteTxt = TextStyle(
+    fontFamily: 'Nunito',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.white);
