66704a18a3c99ca3f12143a29599cd2c15c18d19
mel-fayne <melaniefayne33@gmail.com>
2023-01-17 13:21:38 +0300

    revert home page change

--- lib/core/home/home.dart
+++ lib/core/home/home.dart
@@ -32,44 +32,36 @@ class _HomePageState extends State<HomePage> {
         appBar: mainAppBar(pageTitle: 'Nawiri'),
         drawer: mainDrawer(),
         body: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-            child:
-                //  SizedBox(
-                //     height: double.maxFinite,
-                //     child: navMenu(navItems: [
-                //       navItem(
-                //           iconPath: Icons.point_of_sale,
-                //           label: 'Point of Sale',
-                //           goTo: const PoSPage()),
-                //       navItem(
-                //           iconPath: Icons.sticky_note_2,
-                //           label: 'Transactions',
-                //           goTo: const TransactionsPage()),
-                //       navItem(
-                //           iconPath: Icons.account_balance,
-                //           label: 'Banking',
-                //           goTo: const BankingPage()),
-                //       navItem(
-                //           iconPath: Icons.people,
-                //           label: 'Customers',
-                //           goTo: const CustomersPage()),
-                //       navItem(
-                //           iconPath: Icons.local_shipping_rounded,
-                //           label: 'Suppliers',
-                //           goTo: const SupplierPage()),
-                //       navItem(
-                //           iconPath: Icons.inventory_2,
-                //           label: 'Inventory',
-                //           goTo: const InventoryPage()),
-                //     ])),
-                priBtn(
-                    label: 'sd',
-                    txtColour: Colors.white,
-                    bgColour: kDarkGreen,
-                    isLoading: _isLoading,
-                    function: () {
-                      Get.dialog(const WelcomePrompt());
-                    })));
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          child: SizedBox(
+              height: double.maxFinite,
+              child: navMenu(navItems: [
+                navItem(
+                    iconPath: Icons.point_of_sale,
+                    label: 'Point of Sale',
+                    goTo: const PoSPage()),
+                navItem(
+                    iconPath: Icons.sticky_note_2,
+                    label: 'Transactions',
+                    goTo: const TransactionsPage()),
+                navItem(
+                    iconPath: Icons.account_balance,
+                    label: 'Banking',
+                    goTo: const BankingPage()),
+                navItem(
+                    iconPath: Icons.people,
+                    label: 'Customers',
+                    goTo: const CustomersPage()),
+                navItem(
+                    iconPath: Icons.local_shipping_rounded,
+                    label: 'Suppliers',
+                    goTo: const SupplierPage()),
+                navItem(
+                    iconPath: Icons.inventory_2,
+                    label: 'Inventory',
+                    goTo: const InventoryPage()),
+              ])),
+        ));
   }
 }
 
